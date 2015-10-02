253dff744e7b69969f104baf832ad85cfeed4f16
Raul Zuniga <rzuniga64@gmail.com>
2015-10-02 07:59:04 -0500

    Updated join.html

---
+++ js/validation.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Raul Zuniga on 10/02/2015.
+ */
+document.write("<table id='validate'><tr><td>"
+    + "<a href=\"http://validator.w3.org/check/referer\"> "
+    + "Conformance<br />Checker</a><br />"
+    + "<a href=\"http://validator.w3.org/checklink/\" "
+    + "onclick=\"window.open(this.href,'_blank');return false;\">Link Checker<a></td>");
+
+document.write("<td><a href=\"http://jigsaw.w3.org/css-validator/check/referer\">"
+    + "<img src=\"http://www.austincc.edu/jscholl/images/vcss.png\" alt=\"Valid CSS!\" height=\"31\" width=\"88\" "
+    + "style=\"border: 0px;\" /></a></td></tr></table>");
