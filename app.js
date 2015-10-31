24d899b1f3ec454978147e87436fd3948a52079d
Luke D'Alfonso <pokemonmegaman@yahoo.com>
2015-10-31 21:56:19 +0000

    Changed Node.js code to use MySQL insted of SQLite

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('data.sqlite');
+
+var mysql = require('mysql');
+var sql = mysql.createConnection({
+    host : 'localhost',
+    user : 'pokemonmegaman',
+    database : 'txstexe'
+});
+sql.connect();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-//SQL Code
-db.serialize(function(){
-    db.run("CREATE TABLE IF NOT EXISTS members (id INTEGER PRIMARY KEY,fname TEXT, initial TEXT, lname TEXT, email TEXT, major TEXT, classification TEXT, grad_date INTEGER, tshirt TEXT)");
-});
-
+//Static files code
 app.use(express.static(__dirname));
 
+//Handles membership signup submits.
 app.post('/submit', function(req, res) {
     console.log("Recieved entry with value " + 
         req.param('fname') + ',' +
@@ -23,16 +26,31 @@ app.post('/submit', function(req, res) {
         req.param('email') + ',' +
         req.param('major') + ',' +
         req.param('classification') + ',' +
+        req.param('grad_date') + ',' +
         req.param('tshirt'));
-    db.run("INSERT INTO members (fname, initial, lname, email, major, classification, tshirt) VALUES ($fname, $initial, $lname, $email, $major, $classification, $tshirt)",{
-        $fname: req.param('fname'),
-        $initial: req.param('initial'),
-        $lname: req.param('lname'),
-        $email: req.param('email'),
-        $major: req.param('major'),
-        $classification: req.param('classification'),
-        $tshirt: req.param('tshirt')
-    });
+        
+        //Checks to see if we already have a user with the entered email
+        sql.query("SELECT email FROM Member WHERE email=?", req.param('email'), function(error, rows, fields) {
+           if (rows[0] != null) {
+                //TODO: Render "error" page.
+                res.send("ERROR : Email already exists in database!");
+           } else {
+                sql.query("INSERT INTO Member (fname, mi, lname, email, major, class, grad_date, tshirt_size, start_date) VALUES (" +
+                    '"' + req.param('fname') + '",' +
+                    '"' + req.param('initial') + '",' +
+                    '"' + req.param('lname') + '",' +
+                    '"' + req.param('email') + '",' +
+                    '"' + req.param('major') + '",' +
+                    '"' + req.param('classification') + '",' +
+                    '"' + req.param('grad_date') + '",' +
+                    '"' + req.param('tshirt') + '",' +
+                    getFormattedDate() + ')'
+             );
+             //TODO: Render "success" page.
+             res.send("Signup successful!");
+           }
+        });
+    
 });
 
 //Starts the server
@@ -40,3 +58,13 @@ var server = app.listen(process.env.port || 8080, process.env.IP || "0.0.0.0", f
   var addr = server.address();
   console.log('listening on', addr.address + ':' + addr.port);
 });
+
+function getFormattedDate() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    return year + '-' + month + '-' + day;
+}
