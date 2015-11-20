b607beaef89711686583150dec6432a95841084c
pokemonmegaman@yahoo.com <pokemonmegaman@yahoo.com>
2015-11-20 02:51:18 +0000

    Added error supressions for known, unresolvable problems, fixed other errors.

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,34 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
     // Project configuration. 
     grunt.initConfig({
         jshint: {
             all: ['./*.js']
         },
         swig: {
-        development: {
-          init: {
-              autoescape: true
-          },
-          dest: "www/",
-          src: ['static/**/*.html'],
-          generateSitemap: true,
-          generateRobotstxt: true,
-          siteUrl: 'http://mydomain.net/',
-          production: false,
-          fb_appid: '1349v',
-          ga_account_id: 'UA-xxxxxxxx-1',
-          robots_directive: 'Disallow /',
-          sitemap_priorities: {
-              '_DEFAULT_': '0.5',
-              'index.html': '0.8',
-              'subpage.html': '0.7'
-          }
+            development: {
+                init: {
+                    autoescape: true
+                },
+                dest: "www/",
+                src: ['static/**/*.html']
+            },
+            html: {
+                all: ["static/**/*.html"]
+            }
         },
-        html: {
-            all: ["static/**/*.html"]
-        }
-    }, htmllint: {
-            all: ["www/**/*.html"]
+        htmllint: {
+            all: ["www/**/*.html"],
+            options: {
+                ignore: ['The “scrolling” attribute on the “iframe” element is obsolete. Use CSS instead.',  //thrown by embedded calendars, kept for IE compatibility
+                        'Empty heading.', //thrown by unpopulated templates, namely profile pages.  eventually remove.
+                        'The “date” input type is not supported in all browsers. Please be sure to test, and consider using a polyfill.'] //to be solved, but suppress for now.
+            }
         }
     });
-    
+
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-swig');
     grunt.loadNpmTasks('grunt-html');
-    
+
     grunt.registerTask('default', ['jshint', 'swig', 'htmllint']);
 };
