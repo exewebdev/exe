34f2ecf5bc87c9246e51e4262790d470bd221850
Raul Zuniga <rzuniga64@gmail.com>
2015-11-20 20:54:10 -0600

    Implementation of the navbar dropdown by Kevin.

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var config = {
     //Server config
     host : "0.0.0.0",
-    port : "8080",
+    port : "80",
     secret : "supersecretsessionkey",
     //SQL Config
     sql : {
-        enabled: true,
+        enabled: false,
         host : "localhost",
         user : "pokemonmegaman",
         password : "",
         dbname : "txstexe"
     },
     //The following should absouletely be turned on in production, and is only if your computer doesn't support bcrypt.
-    bcrypt: true
+    bcrypt: false
 };
 
 module.exports = config;
