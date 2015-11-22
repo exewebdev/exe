d17dea7ca9c5a78ccd62a47c864e041994dfb57f
pokemonmegaman@yahoo.com <pokemonmegaman@yahoo.com>
2015-11-22 05:12:45 +0000

    Fixing config file

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var config = {
     //Server config
     host : "localhost",
-    port : "80",
+    port : "8080",
     secret : "supersecretsessionkey",
     //SQL Config
     sql : {
-        enabled: false,
+        enabled: true,
         host : "localhost",
-        user : "txstexe",
-        password : "aggies92",
+        user : "your-username",
+        password : "",
         dbname : "txstexe"
     },
     //The following should absouletely be turned on in production, and is only if your computer doesn't support bcrypt.
-    bcrypt: false
+    bcrypt: true
 };
 
 module.exports = config;
