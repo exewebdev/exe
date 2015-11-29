d131ca00efee8068cb6a78f22620c3b1fa8b1e01
Raul Zuniga <rzuniga64@gmail.com>
2015-11-29 06:12:49 -0600

    Finally got the officer images.  Updated officer.html with new images.

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var config = {
     //Server config
-    host : "localhost",
-    port : "8080",
+    host : "127.0.0.1",
+    port : "80",
     secret : "supersecretsessionkey",
     //SQL Config
     sql : {
         enabled: true,
-        host : "localhost",
-        user : "pokemonmegaman",
-        password : "",
+        host : "127.0.0.1",
+        user : "txstexe",
+        password : "aggies92",
         dbname : "txstexe"
     },
     //The following should absouletely be turned on in production, and is only if your computer doesn't support bcrypt.
