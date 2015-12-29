7d0036dd44399d8733bc0001f25866617c9da452
Raul Zuniga <rzuniga64@gmail.com>
2015-12-29 01:07:44 -0600

    Fix for issue #16 Make the carousel in the home page responsive

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ var config = {
     sql : {
         enabled: true,
         host : "localhost",
-        user : "pokemonmegaman",
-        password : "",
+        user : "txstexe",
+        password : "aggies92",
         dbname : "txstexe"
     },
     facebook : {
